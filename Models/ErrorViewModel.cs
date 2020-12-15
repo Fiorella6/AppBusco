0b93b38eaae7ebc734fc01debad8efe2e7e2010b
Fiorella Diaz <fiorelladiazmeza@gmail.com>
2020-12-15 08:53:54 -0500

    Creación de una nueva Carpeta

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace AppBusco.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}

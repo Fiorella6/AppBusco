0b7f0af47ae87a9e7b92e2ac030e7bf6f8a40355
Fiorella Diaz <fiorelladiazmeza@gmail.com>
2020-12-15 09:54:51 -0500

    LLenado de Registro

--- Models/Registro.cs
+++ Models/Registro.cs
@@ -1,7 +1,35 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace AppBusco.Models
 {
     public class Registro
     {
+         [Required]
+
+      public string Nombre_del_Producto { get; set;}
+
+        [Required]
+
+        public string Url_Producto { get; set;}
+
+          [Required]
+
+          public string Descripcion { get; set;}
+
+            [Required]
+
+            public string Precio  {get; set;}
         
+              [Required]
+
+              public string Celular { get; set;}
+
+                [Required]
+
+                public string Lugar_De_Compra { get; set;}
+
+                  [Required]
+
+                  public string Nombre { get; set;}
     }
 }
